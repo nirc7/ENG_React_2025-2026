3817fe0a606aef8fe5d4a02eb633e4538763de31
nc <31476486+nirc7@users.noreply.github.com>
2025-10-29 11:14:43 +0200

    lec1

---
+++ Lec01 29_10_2025 Fund/v1-demo/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import EHeader from './Elements/EHeader';
+
+
+function App() {
+
+  return (
+
+    <div>{EHeader}
+      Hello World! <br />
+      <div>asdasdf</div>
+    </div>
+
+  )
+}
+
+export default App
---
+++ Lec01 29_10_2025 Fund/v1-demo/src/Elements/EHeader.jsx
@@ -0,0 +1,8 @@
+
+const EHeader =
+  <div>
+    <div>Header Ruppin!</div>
+    <div>Header Ruppin!2</div>
+  </div>;
+
+export default EHeader;
