31bda4e7a927ebb37118c8b52d4f3a1092a68904
nc <31476486+nirc7@users.noreply.github.com>
2025-11-19 10:50:22 +0200

    bootsratp

--- Lec05+1 18_11_2025 Uplifting/calc-comps-demo/src/ClassComps/CCInputs.jsx
+++ Lec05+1 18_11_2025 Uplifting/calc-comps-demo/src/ClassComps/CCInputs.jsx
@@ -35,15 +35,29 @@ export default class CCInputs extends Component {
         padding: 10,
         borderRadius: radius,
         color: 'purple',
-        fontFamily: 'bold',
-        fontSize: 30
+        // fontFamily: 'bold',
+        // fontSize: 30
       }}>CCinputs <br />
 
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="inputGroup-sizing-default">NUM1:</span>
+          <input type="text" className="form-control"
+            onChange={this.chgNum1}
+            aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+        </div>
+        <div className="form-floating mb-3">
+          <input type="text" className="form-control"
+            onChange={this.chgNum2}
+            id="floatingInput" placeholder='num2...' />
+          <label htmlFor="floatingInput">num2...</label>
+        </div>
         num1: <input type="number"
           placeholder="enter num1" onChange={this.chgNum1} /> <br />
         num2: <input type="text"
           placeholder="enter num2" onChange={this.chgNum2} /> <br />
-        <button onClick={this.btnSum}>+</button>
+        <button type="button"
+          onClick={this.btnSum}
+          className="btn btn-outline-success">+</button>
       </div>
     )
   }
